c7336b4477d7d6f520e53bf29f74c85c093734d7
Jose Diaz <jozz.18x@gmail.com>
2018-05-10 07:40:46 -0500

    Update

--- js/constants.js
+++ js/constants.js
@@ -3,7 +3,7 @@
  * Shared preference for collapse the page.
  * @const
  */
-export let Constants = {
+export const Constants = {
     PREF_COLLAPSE_PAGE: 'PREF_COLLAPSE_PAGE',
     PREF_LAST_SELECTED_ITEM: 'PREF_LAST_ITEM_SELECTED'
 };
